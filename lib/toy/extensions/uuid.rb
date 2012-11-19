2d5b932667ee8ac1374405882d755b568ff65972
John Nunemaker <nunemaker@gmail.com>
2012-11-19 13:19:20 -0500

    Ensure uuid returns self if already a uuid.

--- lib/toy/extensions/uuid.rb
+++ lib/toy/extensions/uuid.rb
@@ -2,10 +2,12 @@ module Toy
   module Extensions
     module UUID
       def to_store(value, *)
+        return value if value.is_a?(SimpleUUID::UUID)
         SimpleUUID::UUID.new(value)
       end
 
       def from_store(value, *)
+        return value if value.is_a?(SimpleUUID::UUID)
         SimpleUUID::UUID.new(value)
       end
     end
--- spec/toy/extensions/uuid_spec.rb
+++ spec/toy/extensions/uuid_spec.rb
@@ -1,18 +1,28 @@
 require 'helper'
 
 describe "SimpleUUID::UUID.to_store" do
-  it "should convert value to uuid" do
+  it "returns value if value is already uuid" do
     uuid = SimpleUUID::UUID.new
-    [uuid, uuid.to_guid, uuid.to_s, uuid.to_i].each do |value|
+    SimpleUUID::UUID.to_store(uuid).should be(uuid)
+  end
+
+  it "converts value to uuid if not already uuid" do
+    uuid = SimpleUUID::UUID.new
+    [uuid.to_guid, uuid.to_s, uuid.to_i].each do |value|
       SimpleUUID::UUID.from_store(value).should eq(uuid)
     end
   end
 end
 
 describe "SimpleUUID::UUID.from_store" do
-  it "should convert value to uuid" do
+  it "returns value if value is already uuid" do
     uuid = SimpleUUID::UUID.new
-    [uuid, uuid.to_guid, uuid.to_s, uuid.to_i].each do |value|
+    SimpleUUID::UUID.from_store(uuid).should be(uuid)
+  end
+
+  it "converts value to uuid if not already uuid" do
+    uuid = SimpleUUID::UUID.new
+    [uuid.to_guid, uuid.to_s, uuid.to_i].each do |value|
       SimpleUUID::UUID.from_store(value).should eq(uuid)
     end
   end
